1603963f07c1820aa7371fb663df77ea85d23f9e
Stan Svec <stan@taggstar.com>
2015-09-15 16:54:01 +0100

    0xDBE script fixed

--- 0xdbe.sh
+++ 0xdbe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FILES=$HOME/Installation/files
-rm -rf /opt/0xdbe
+rm -rf /opt/0xDBE
 tar -xvf $FILES/0xdbe/0xdbe.tar.gz -C /opt
 mv /opt/0xDBE-* /opt/0xDBE
 ln -sf /opt/0xDBE/bin/0xdbe.sh /usr/bin/0xdbe.sh
